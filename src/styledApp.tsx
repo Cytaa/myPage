23a3a38692c4a9c8f64a59bd861c3e044d2ab46d
Piotr Stachnio <piotrstachnio@gmail.com>
2023-01-14 18:03:08 +0100

    elo

---
+++ src/styledApp.tsx
@@ -0,0 +1,13 @@
+import {createGlobalStyle} from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+    body {
+        font: 10px ,
+        background-color: black,
+        margin: 0,
+        width: 100%,
+        height: 100%,
+    }
+`
+
+export {GlobalStyle}
